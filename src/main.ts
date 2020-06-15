a5b875c65a9a9f96038ec67ef390a49d6b920d14
Jan Dietrich <jan.dietrich@tecpose.com>
2020-06-15 02:04:20 +0200

    added repo list

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import "../styles/main.scss";
 
 console.log("started");
 
+
+( () => {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0] as HTMLElement;
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+})()
+
 import {Bubble} from "./visualizations/bubbles";
 
 const initialState = {
@@ -10,6 +26,7 @@ const initialState = {
     dimension2: 'num_repos',
     dimension3: 'num_members'
 }
+const state: any = initialState;
  
 loadData();
 
@@ -26,26 +43,62 @@ function loadData() {
 function redraw() {
 }
 
+let bubble: Bubble;
 function setupVisualizations(data) {
+   state.dimensions = Object.keys(data[0]);
    createDimensionSelection(data, initialState)
-   Bubble.setup(data);
+   state.data = data;
+   bubble = new Bubble();
+   bubble.setup(state);
+   bubble.onselect = (institution: any) => {
+      repoList(institution);
+   };
 }
 
+const dimensionSelectElements  = [
+    document.getElementById("dimension1") as HTMLSelectElement,
+    document.getElementById("dimension2") as HTMLSelectElement,
+    document.getElementById("dimension3") as HTMLSelectElement,
+];
+
 function createDimensionSelection(data, initialState) {
-    const dimensions = Object.keys(data[0]);
-    const dimensionSelectElements  = [
-        document.getElementById("dimension1") as HTMLSelectElement,
-        document.getElementById("dimension2") as HTMLSelectElement,
-        document.getElementById("dimension3") as HTMLSelectElement,
-    ];
     dimensionSelectElements.forEach( (el, i) => {
         while (el.firstChild) {
             el.removeChild(el.lastChild);
         }
-        dimensions.forEach( dimension => {
+        state.dimensions.forEach( dimension => {
             el.append(new Option(dimension, dimension));
         })
-        el.selectedIndex = dimensions.indexOf(initialState["dimension"+(i+1)]);
+        el.selectedIndex = state.dimensions.indexOf(initialState["dimension"+(i+1)]);
+        el.onchange = selectDimension;
+    })
+}
+
+function selectDimension() {
+    dimensionSelectElements.forEach( (el, i) => {
+        state['dimension'+ (i+1)] = state.dimensions[el.selectedIndex];
+    });
+    bubble.update(state);
+}
+
+function repoList(institution) {
+    const el = document.getElementById('repoList');
+    while (el.firstChild) {
+        el.removeChild(el.lastChild);
+    }
+    const repos = JSON.parse(institution.repo_names.replace(/'/g, '"'));
+    const orgs = JSON.parse(institution.orgs.replace(/'/g, '"'));
+    repos.forEach( repo => {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = 'https://github.com/' + orgs[0] + '/' + repo;
+        a.target = '_blank';
+        const content = document.createTextNode(repo)
+        a.appendChild(content);
+        li.appendChild(a);
+        el.append(li);
     })
     
+    const modal = document.getElementById("myModal");
+    modal.style.display = "block";
 }
