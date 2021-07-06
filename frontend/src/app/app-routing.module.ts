bc73ca04188ffd30a8bc7db9fa889127527114e8
titfortat265358 <lionel@stuermer.ch>
2021-07-06 11:12:34 +0200

    Added ranking table

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {ListComponent} from './metric/list/list.component';
+import {RankingComponent} from './ranking/ranking.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'metrics',
     component: ListComponent
+  },
+  {
+    path: 'ranking',
+    component: RankingComponent
   }
 ];
 
---
+++ frontend/src/app/ranking/ranking.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RankingComponent } from './ranking.component';
+
+describe('RankingComponent', () => {
+  let component: RankingComponent;
+  let fixture: ComponentFixture<RankingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RankingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RankingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
