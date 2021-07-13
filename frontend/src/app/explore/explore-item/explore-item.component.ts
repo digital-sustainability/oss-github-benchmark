8e8a010fed4ed20af86af22d7a320edaaafbb14f
titfortat265358 <lionel@stuermer.ch>
2021-07-13 16:32:57 +0200

    Tons of small changes.

--- frontend/src/app/explore/explore-item/explore-item.component.ts
+++ frontend/src/app/explore/explore-item/explore-item.component.ts
@@ -1,58 +1,71 @@
-import { Component, OnInit, AfterViewInit, Inject, ViewChild } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {IInstitution} from 'src/app/interfaces/institution';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatSort, Sort} from '@angular/material/sort';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Inject,
+  ViewChild,
+} from '@angular/core';
+import { IInstitution } from 'src/app/interfaces/institution';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort, Sort } from '@angular/material/sort';
 import { lowerCase } from 'lodash-es';
-import {MatPaginator} from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-const sortState: Sort = {active: 'name', direction: 'asc'};
+const sortState: Sort = { active: 'name', direction: 'desc' };
 
 @Component({
   selector: 'app-explore-item',
   templateUrl: './explore-item.component.html',
-  styleUrls: ['./explore-item.component.scss']
+  styleUrls: ['./explore-item.component.scss'],
 })
-
 export class ExploreItemComponent implements OnInit, AfterViewInit {
   item: IInstitution;
-  displayedColumns: string[] = ['name', 'num_commits', 'num_contributors', 'num_watchers'];
+  displayedColumns: string[] = [
+    'name',
+    'num_commits',
+    'num_contributors',
+    'num_watchers',
+    'fork',
+    'num_stars',
+    'num_forks',
+  ];
   dataSource: any = 0;
 
   constructor(
     private dialogRef: MatDialogRef<ExploreItemComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    this.sort = new MatSort;
+    this.sort = new MatSort();
   }
- 
+
   ngOnInit(): void {
     if (this.data.repos) {
-      this.data.repos.forEach(repo => {
+      this.data.repos.forEach((repo) => {
         repo.name = lowerCase(repo.name);
       });
     }
-    if (typeof(this.data.avatar) == "string") {
-      this.data.avatar = JSON.parse(this.data.avatar.replace(/\'/g, "\""));
-    };
+    if (typeof this.data.avatar == 'string') {
+      this.data.avatar = JSON.parse(this.data.avatar.replace(/\'/g, '"'));
+    }
     if (this.data.repos.length > 0) {
       this.dataSource = new MatTableDataSource(this.data.repos);
-    };
-  };
-
-  navigateTo(url: string): void {
-    window.open(url, "_blank");
-  }
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: false}) sort: MatSort;
-
-  ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
+    }
     this.sort.active = sortState.active;
     this.sort.direction = sortState.direction;
     this.sort.sortChange.emit(sortState);
+    console.log(this.data.repos);
+  }
+
+  navigateTo(url: string): void {
+    window.open(url, '_blank');
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
   }
 }
