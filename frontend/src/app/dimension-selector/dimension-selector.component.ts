c12064eb3e0d33b48dcf15e83370306a34e5f8fe
titfortat265358 <lionel@stuermer.ch>
2021-07-14 19:01:49 +0200

    Made everything prettier with Prettier.

--- frontend/src/app/dimension-selector/dimension-selector.component.ts
+++ frontend/src/app/dimension-selector/dimension-selector.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
-import {DataService, Metric} from '../data.service';
-import {Options} from '../visualizations/options';
+import { FormBuilder } from '@angular/forms';
+import { DataService, Metric } from '../data.service';
+import { Options } from '../visualizations/options';
 
 @Component({
   selector: 'app-dimension-selector',
   templateUrl: './dimension-selector.component.html',
-  styleUrls: ['./dimension-selector.component.scss']
+  styleUrls: ['./dimension-selector.component.scss'],
 })
 export class DimensionSelectorComponent implements OnInit {
-
   dimensionForm = this.fb.group({
     dimension1: 'total_num_contributors',
     dimension2: 'num_repos',
@@ -19,28 +18,30 @@ export class DimensionSelectorComponent implements OnInit {
   @Output() optionsChange = new EventEmitter<Options>();
   metrics: Metric[];
 
-  constructor(
-    private dataService: DataService,
-    private fb: FormBuilder
-  ) { }
+  constructor(private dataService: DataService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.dataService.dimensionOptions.subscribe( ops => {
+    this.dataService.dimensionOptions.subscribe((ops) => {
       this.metrics = ops;
       this.updateOptions();
     });
-    this.dimensionForm.valueChanges.subscribe( () => {
+    this.dimensionForm.valueChanges.subscribe(() => {
       this.updateOptions();
     });
   }
 
   updateOptions(): void {
     const options = {
-      dimension1: this.metrics.filter( m => m.key === this.dimensionForm.get('dimension1').value)[0],
-      dimension2: this.metrics.filter( m => m.key === this.dimensionForm.get('dimension2').value)[0],
-      dimension3: this.metrics.filter( m => m.key === this.dimensionForm.get('dimension3').value)[0],
+      dimension1: this.metrics.filter(
+        (m) => m.key === this.dimensionForm.get('dimension1').value
+      )[0],
+      dimension2: this.metrics.filter(
+        (m) => m.key === this.dimensionForm.get('dimension2').value
+      )[0],
+      dimension3: this.metrics.filter(
+        (m) => m.key === this.dimensionForm.get('dimension3').value
+      )[0],
     };
     this.optionsChange.emit(options);
   }
-
 }
--- frontend/src/app/visualizations/sunburst/sunburst.component.ts
+++ frontend/src/app/visualizations/sunburst/sunburst.component.ts
@@ -1,16 +1,15 @@
 import { Component, ElementRef, Input, OnChanges, OnInit } from '@angular/core';
-import {IData} from 'src/app/data.service';
-import {Options} from '../options';
+import { IData } from 'src/app/data.service';
+import { Options } from '../options';
 import * as d3 from 'd3';
-import {ISector} from 'src/app/interfaces/institution';
+import { ISector } from 'src/app/interfaces/institution';
 
 @Component({
   selector: 'app-visualization-sunburst',
   templateUrl: './sunburst.component.html',
-  styleUrls: ['./sunburst.component.scss']
+  styleUrls: ['./sunburst.component.scss'],
 })
 export class SunburstComponent implements OnInit, OnChanges {
-
   @Input() data: IData;
   @Input() options: Options;
   svg: any;
@@ -27,7 +26,7 @@ export class SunburstComponent implements OnInit, OnChanges {
     const breadcrumbHeight = 30;
     const tipWidth = 10;
     const points = [];
-    points.push("0,0");
+    points.push('0,0');
     points.push(`${breadcrumbWidth},0`);
     points.push(`${breadcrumbWidth + tipWidth},${breadcrumbHeight / 2}`);
     points.push(`${breadcrumbWidth},${breadcrumbHeight}`);
@@ -36,10 +35,10 @@ export class SunburstComponent implements OnInit, OnChanges {
       // Leftmost breadcrumb; don't include 6th vertex.
       points.push(`${tipWidth},${breadcrumbHeight / 2}`);
     }
-    return points.join(" ");
+    return points.join(' ');
   }
 
-  constructor(private hostElement: ElementRef) { }
+  constructor(private hostElement: ElementRef) {}
 
   ngOnInit(): void {
     const bound = this.hostElement.nativeElement.getBoundingClientRect();
@@ -58,7 +57,8 @@ export class SunburstComponent implements OnInit, OnChanges {
     this.element = this.svg.node();
     this.element.value = { sequence: [], percentage: 0.0 };
 
-    this.arc = d3.arc()
+    this.arc = d3
+      .arc()
       .startAngle((d: any) => d.x0)
       .endAngle((d: any) => d.x1)
       .padAngle(1 / this.radius)
@@ -66,7 +66,8 @@ export class SunburstComponent implements OnInit, OnChanges {
       .innerRadius((d: any) => Math.sqrt(d.y0))
       .outerRadius((d: any) => Math.sqrt(d.y1) - 1);
 
-    this.mousearc = d3.arc()
+    this.mousearc = d3
+      .arc()
       .startAngle((d: any) => d.x0)
       .endAngle((d: any) => d.x1)
       .innerRadius((d: any) => Math.sqrt(d.y0))
@@ -88,8 +89,8 @@ export class SunburstComponent implements OnInit, OnChanges {
     this.label = this.svg
       .append('text')
       .attr('text-anchor', 'middle')
-      .attr('fill', '#888')
-      // .style('visibility', 'hidden');
+      .attr('fill', '#888');
+    // .style('visibility', 'hidden');
 
     this.label
       .append('tspan')
@@ -107,7 +108,7 @@ export class SunburstComponent implements OnInit, OnChanges {
       .attr('y', 0)
       .attr('dy', '4em')
       .attr('font-size', '2em')
-      .text('filler')
+      .text('filler');
 
     this.label
       .append('tspan')
@@ -177,53 +178,49 @@ export class SunburstComponent implements OnInit, OnChanges {
           .select('.percentage')
           .text(percentage + '%');
 
-        this.label
-          .select('.sectionName')
-          .text(d.data.name);
+        this.label.select('.sectionName').text(d.data.name);
         // Update the value of this view with the currently hovered sequence and percentage
         this.element.value = { sequence, percentage };
         this.element.dispatchEvent(new CustomEvent('input'));
       });
-
   }
 
   partition(data) {
     return d3.partition().size([2 * Math.PI, this.radius * this.radius])(
       d3
         .hierarchy(data)
-        .sum((d) => d.total ? d.total : d.value)
+        .sum((d) => (d.total ? d.total : d.value))
         .sort((a, b) => b.value - a.value)
     );
   }
 
   prepareData(data: ISector) {
-      const data1 = Object.keys(data).map( sector => {
-          return {
-              name: sector,
-              children: data[sector].map(inst => {
-                  return {
-                      name: inst.name,
-                      total: inst['total_num_commits'],
-                      children: inst.orgs.map(org => {
-                          return {
-                              name: org.name,
-                              total: org['total_num_commits'],
-                              children: org.repos.map( repo => {
-                                return {
-                                  name: repo.name,
-                                  total: org['num_commits']
-                                }
-                              })
-                          };
-                      })
-                  };
-              })
-          };
-      });
+    const data1 = Object.keys(data).map((sector) => {
       return {
-        name: 'root',
-        children: data1
+        name: sector,
+        children: data[sector].map((inst) => {
+          return {
+            name: inst.name,
+            total: inst['total_num_commits'],
+            children: inst.orgs.map((org) => {
+              return {
+                name: org.name,
+                total: org['total_num_commits'],
+                children: org.repos.map((repo) => {
+                  return {
+                    name: repo.name,
+                    total: org['num_commits'],
+                  };
+                }),
+              };
+            }),
+          };
+        }),
       };
+    });
+    return {
+      name: 'root',
+      children: data1,
+    };
   }
-
 }
