8359e9db05c2be5d8fd2a6838d69296bcc705afa
titfortat265358 <lionel@stuermer.ch>
2022-07-28 10:49:41 +0200

    Upgrading Angular to Version 12.

--- frontend/src/app/explore-item/explore-item.component.ts
+++ frontend/src/app/explore-item/explore-item.component.ts
@@ -11,8 +11,8 @@ import { MatSort, Sort } from '@angular/material/sort';
 import { lowerCase } from 'lodash-es';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Chart, ChartConfiguration, ChartType } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
+// import { Chart, ChartConfiguration, ChartType } from 'chart.js';
+// import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-explore-item',
@@ -20,97 +20,97 @@ import { BaseChartDirective } from 'ng2-charts';
   styleUrls: ['./explore-item.component.scss'],
 })
 export class ExploreItemComponent implements OnInit {
-  public lineChartData: ChartConfiguration['data'] = {
-    datasets: [
-      {
-        data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      },
-      {
-        data: [28, 48, 40, 19, 86, 27, 90],
-        label: 'Series B',
-        backgroundColor: 'rgba(77,83,96,0.2)',
-        borderColor: 'rgba(77,83,96,1)',
-        pointBackgroundColor: 'rgba(77,83,96,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(77,83,96,1)',
-        fill: 'origin',
-      },
-      {
-        data: [180, 480, 770, 90, 1000, 270, 400],
-        label: 'Series C',
-        yAxisID: 'y-axis-1',
-        backgroundColor: 'rgba(255,0,0,0.3)',
-        borderColor: 'red',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      },
-    ],
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  };
+  // public lineChartData: ChartConfiguration['data'] = {
+  //   datasets: [
+  //     {
+  //       data: [65, 59, 80, 81, 56, 55, 40],
+  //       label: 'Series A',
+  //       backgroundColor: 'rgba(148,159,177,0.2)',
+  //       borderColor: 'rgba(148,159,177,1)',
+  //       pointBackgroundColor: 'rgba(148,159,177,1)',
+  //       pointBorderColor: '#fff',
+  //       pointHoverBackgroundColor: '#fff',
+  //       pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+  //       fill: 'origin',
+  //     },
+  //     {
+  //       data: [28, 48, 40, 19, 86, 27, 90],
+  //       label: 'Series B',
+  //       backgroundColor: 'rgba(77,83,96,0.2)',
+  //       borderColor: 'rgba(77,83,96,1)',
+  //       pointBackgroundColor: 'rgba(77,83,96,1)',
+  //       pointBorderColor: '#fff',
+  //       pointHoverBackgroundColor: '#fff',
+  //       pointHoverBorderColor: 'rgba(77,83,96,1)',
+  //       fill: 'origin',
+  //     },
+  //     {
+  //       data: [180, 480, 770, 90, 1000, 270, 400],
+  //       label: 'Series C',
+  //       yAxisID: 'y-axis-1',
+  //       backgroundColor: 'rgba(255,0,0,0.3)',
+  //       borderColor: 'red',
+  //       pointBackgroundColor: 'rgba(148,159,177,1)',
+  //       pointBorderColor: '#fff',
+  //       pointHoverBackgroundColor: '#fff',
+  //       pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+  //       fill: 'origin',
+  //     },
+  //   ],
+  //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  // };
 
-  public lineChartOptions: ChartConfiguration['options'] = {
-    elements: {
-      line: {
-        tension: 0.5,
-      },
-    },
-    scales: {
-      // We use this empty structure as a placeholder for dynamic theming.
-      x: {},
-      'y-axis-0': {
-        position: 'left',
-      },
-      'y-axis-1': {
-        position: 'right',
-        grid: {
-          color: 'rgba(255,0,0,0.3)',
-        },
-        ticks: {
-          color: 'red',
-        },
-      },
-    },
+  // public lineChartOptions = {
+  //   elements: {
+  //     line: {
+  //       tension: 0.5,
+  //     },
+  //   },
+  //   scales: {
+  //     // We use this empty structure as a placeholder for dynamic theming.
+  //     x: {},
+  //     'y-axis-0': {
+  //       position: 'left',
+  //     },
+  //     'y-axis-1': {
+  //       position: 'right',
+  //       grid: {
+  //         color: 'rgba(255,0,0,0.3)',
+  //       },
+  //       ticks: {
+  //         color: 'red',
+  //       },
+  //     },
+  //   },
 
-    plugins: {
-      legend: { display: true },
-      annotation: {
-        annotations: [
-          {
-            type: 'line',
-            scaleID: 'x',
-            value: 'March',
-            borderColor: 'orange',
-            borderWidth: 2,
-            label: {
-              position: 'center',
-              enabled: true,
-              color: 'orange',
-              content: 'LineAnno',
-              font: {
-                weight: 'bold',
-              },
-            },
-          },
-        ],
-      },
-    },
-  };
+  //   plugins: {
+  //     legend: { display: true },
+  //     annotation: {
+  //       annotations: [
+  //         {
+  //           type: 'line',
+  //           scaleID: 'x',
+  //           value: 'March',
+  //           borderColor: 'orange',
+  //           borderWidth: 2,
+  //           label: {
+  //             position: 'center',
+  //             enabled: true,
+  //             color: 'orange',
+  //             content: 'LineAnno',
+  //             font: {
+  //               weight: 'bold',
+  //             },
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   },
+  // };
 
-  public lineChartType: ChartType = 'line';
+  // public lineChartType: ChartType = 'line';
 
-  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
+  // @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
   item: any;
   displayedColumns: string[] = [
