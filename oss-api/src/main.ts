e998a5af2da6b25cd393b05a59f6352350acfec7
Joel <joel.roblesgasser@bfh.ch>
2023-08-17 12:30:46 +0200

    wip

--- oss-api/src/main.ts
+++ oss-api/src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
+  app.flushLogs();
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
