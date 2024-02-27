894f1692c6f4652bf4fa68a5c8e7132149336642
anina.hold@students.ffhs.ch <anina.hold@bfh.ch>
2024-02-27 13:19:02 +0100

    test: reduce cron job frequency back to 1 hour

--- oss-api/src/github-crawler/github-crawler.service.ts
+++ oss-api/src/github-crawler/github-crawler.service.ts
@@ -39,7 +39,7 @@ export class GithubCrawlerService {
   /**
    * Prepare all the institution data
    */
-  @Cron(CronExpression.EVERY_5_MINUTES)
+  @Cron(CronExpression.EVERY_HOUR)
   private async prepareInstitutions() {
     this.logger.log(`Prepairing all institutions to be crawled`);
     this.reachedGithubCallLimit = false;
