494f7f14f4b57138561d1dfa7a9f020a97028651
hoa3 <anina.hold@outlook.com>
2024-05-29 14:56:45 +0200

    fix: cleanup authentication

--- oss-api/src/auth/constants.ts
+++ oss-api/src/auth/constants.ts
@@ -1,5 +1,6 @@
+import 'dotenv/config';
+
 export const jwtConstants = {
-  //TODO: change sectret, save it in an appropriate way, anina
-  secret:
-    'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+  //TODO: change sectret, save it in an appropriate way, anina schauen ob dieses überhaupt gebraucht wird
+  secret: process.env.JWT_SECRET,
 };
